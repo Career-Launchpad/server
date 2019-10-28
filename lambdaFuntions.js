256586cd259fde0cfd48c0101be03bb921ef1499
Bo Bleyl <boaustinbleyl@gmail.com>
2019-10-28 17:51:44 -0600

    Added endpoint for adding student

--- lambdaFuntions.js
+++ lambdaFuntions.js
@@ -50,3 +50,21 @@ exports.handler = (event, context, callback) => {
     })
   })
 };
+
+// "create_student" Function
+// Post request: https://d23kwuyo38.execute-api.us-east-1.amazonaws.com/production/student
+const mysql = require('mysql');
+const con = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+exports.handler = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const sql = `INSERT INTO launchpad.student (id, school_id, major) VALUES ('${event.StudentID}', (SELECT y.id FROM launchpad.school y WHERE y.name = '${event.School}'), '${event.Major}')`
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    callback(null, (result))
+  });
+};
