aff4b2fb85f8a197d1a69fd9b7ea84988e835a0c
Justin Hendrickson <justintunev7@gmail.com>
2020-02-20 10:37:15 -0700

    configure filtering multiple values

--- src/graphql/resolvers/getStudentsResolver.js
+++ src/graphql/resolvers/getStudentsResolver.js
@@ -1,7 +1,8 @@
 import { TABLES } from "../environment";
 import { dbScan } from "./resolverHelper";
-
-// Gets all students
+/*
+ * Gets a list of students
+ */
 const GetStudentsResolver = async (db, args) => {
   const res = await dbScan(db, TABLES.Student, args.filters);
   return { edges: res };
