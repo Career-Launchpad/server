898372b08dc87661d783983627884f934acac68e
Bo Bleyl <boaustinbleyl@gmail.com>
2020-03-10 10:08:31 -0600

    Auth.js file

--- lambdas/auth.js
+++ lambdas/auth.js
@@ -6,13 +6,33 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.auth = async (event, context, callback) => {
   const params = { TableName: "Aliases" };
-  let aliasList = await dynamoDb.scan(params).promise();
-  aliasList = aliasList.Items;
+  //let aliasList = await dynamoDb.scan(params).promise();
+  //aliasList = aliasList.Items;
 
-  response = {
-    statusCode: 200,
-    body: JSON.stringify(aliasList)
-  };
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't fetch the todo item."
+      });
+      return;
+    }
 
-  callback(null, response);
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item)
+    };
+    callback(null, response);
+  });
+
+  //response = {
+  //statusCode: 200,
+  //body: JSON.stringify(aliasList)
+  //};
+
+  //callback(null, response);
 };
